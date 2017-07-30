b54b2b3b3d72d2b72ce823acc3637a1f53b6ee71
jspc <james@zero-internet.org.uk>
2017-07-30 12:57:04 +0100

    Use sqlx

--- database.go
+++ database.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"database/sql"
+	"github.com/jmoiron/sqlx"
 
 	_ "github.com/lib/pq"
 	_ "github.com/satori/go.uuid"
 )
 
 type database struct {
-	db *sql.DB
+	db *sqlx.DB
 }
 
 type User struct {
@@ -41,45 +41,19 @@ const (
 )
 
 func NewDatabase(connection string) (d database, err error) {
-	d.db, err = sql.Open("postgres", connection)
+	d.db, err = sqlx.Connect("postgres", connection)
 
 	return
 }
 
 func (d database) GetNearbyLocations(userID string, long, lat float64) (l []Location, err error) {
-	rows, err := d.db.Query("SELECT * FROM locations WHERE userid = $1 AND long > $2 AND long < $3 AND lat > $4 AND lat < $5",
+	err = d.db.Select(&l, "SELECT * FROM locations WHERE userid = $1 AND long > $2 AND long < $3 AND lat > $4 AND lat < $5",
 		userID,
 		long-0.001,
 		long+0.001,
 		lat-0.001,
 		lat+0.001,
 	)
-
-	if err != nil {
-		return
-	}
-
-	defer rows.Close()
-	if err = rows.Err(); err != nil {
-		return
-	}
-
-	for rows.Next() {
-		var loc Location
-
-		err = rows.Scan(
-			&loc.UUID,
-			&loc.UserID,
-			&loc.Name,
-			&loc.Long,
-			&loc.Lat,
-		)
-		if err != nil {
-			return
-		}
-
-		l = append(l, loc)
-	}
 
 	return
 }
